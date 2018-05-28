5d5cede82d3b58d8e90e155ee43a43e39c8c7cd2
Cian Woods <cianwoods@me.com>
2018-05-28 22:50:54 +0100

    Use cors

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
-const app = express();
+const cors = require('cors')
 const axios = require('axios');
+const app = express();
+
+app.use(cors());
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
