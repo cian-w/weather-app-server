7a98786f9569dc7e21b8eb423c4ef5ff948f8f4c
Cian Woods <cianwoods@me.com>
2018-05-28 21:55:48 +0100

    Get weather from Dark Sky

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
 const express = require('express');
 const app = express();
+const axios = require('axios');
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'));
+app.get('/forecast/:location', (req, res) => {
+  let location = req.params.location;
+  
+  
+  axios.get(`https://api.darksky.net/forecast/${process.env.DARK_SKY_API_KEY}/${location}?units=ca`)
+  .then(response => {
+    let forecast = response.data;
+    res.send(forecast);
+  })
+  .catch(error => {
+    console.log(error);
+  });
+});
+
+app.listen(3000, () => console.log('Weather app server running on port 3000!'));
